601fb8f62efbef5e2c1e9b4ff9f1a344b084f86f
mcmf <mcmf@G4C28.windows.cin.ufpe.br>
2017-11-16 14:10:45 -0300

    tou adicionando um emoticon

--- src/pacoteClassesUsuario/CadastroUsuario.java
+++ src/pacoteClassesUsuario/CadastroUsuario.java
@@ -3,7 +3,7 @@ package pacoteClassesUsuario;
  * "classe colecao de negocio"
  * ainda nao entendi pra que vai servir essa classe
  * @author Gabriel
- *
+ * u.u
  */
 public class CadastroUsuario {
 
