070ab42c8cb7782cba3d099e2f1915c953b8125c
melis <melis@LAPTOP-T65EBPGC>
2017-11-21 02:37:17 -0200

    teste2

--- src/pacoteClassesUsuario/RepositorioUsuarios.java
+++ src/pacoteClassesUsuario/RepositorioUsuarios.java
@@ -1,10 +1,12 @@
 package pacoteClassesUsuario;
-/**
- * "interface negocio-dados"
- * interface implementada em RepositrioUsuariosArray e RepositorioUsuariosLista
- * @author Gabriel
- *
- */
-public interface RepositorioUsuarios {
 
+public interface RepositorioUsuarios {
+	
+	public void inserir(Usuario usuario);
+
+	public void remover(String nomeUsuario);
+
+	public void atualizar(Usuario usuario);
+
+	public boolean existe(String nomeUsuario);
 }
