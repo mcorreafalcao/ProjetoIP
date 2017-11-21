6ae9528976c92b0a4a811bb1bcf72a422d063ea3
melis <melis@LAPTOP-T65EBPGC>
2017-11-21 02:29:31 -0200

    teste13

--- src/pacoteClassesUsuario/Usuario.java
+++ src/pacoteClassesUsuario/Usuario.java
@@ -1,9 +1,40 @@
 package pacoteClassesUsuario;
-/**
- * "classe basica"
- * @author Gabriel
- *
- */
+
 public class Usuario {
+
+
+	private String nick;
+	private String email;
+	private String senha;
+
+	public Usuario() {
+		this.nick = null;
+		this.email = null;
+		this.senha = null;
+	}
+	
+	public String getNick() {
+		return nick;
+	}
+	
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
