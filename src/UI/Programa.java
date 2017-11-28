46dd049cd4d30331b08915df3f37b98696da8d25
zegabr <jgsp2@cin.ufpe.br>
2017-11-28 09:03:47 -0400

    coment inutil retirado do topo da classe programa

--- src/UI/Programa.java
+++ src/UI/Programa.java
@@ -1,11 +1,5 @@
 package UI;
 
-/**
- *	VER COMENTARIO ABAIXO
- * 
- * Lembrar de colocar todas as atribuicoes de op em um try catch, pois pode gerar InputMismatchEsception.
- * Ou isso é desnecessário??
- */
 
 import java.util.Scanner;
 
